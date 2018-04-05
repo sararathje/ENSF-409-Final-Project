7d09bb3585a4e3f0e9f2dce0858663dd728e41a9
Sara Rathje <sararathje@Saras-MacBook-Air-2.local>
2018-04-05 17:14:12 -0600

    More constants

---
+++ Constants/MessageConstants.java
@@ -0,0 +1,13 @@
+package Constants;
+
+/**
+ * Defines the message constants for learning platform GUI.
+ *
+ * @author Jack Glass, Rylan Kettles, Sara Rathje
+ * @version 1.0
+ * @since April 3, 2018
+ */
+public interface MessageConstants {
+    String EMPTY_LOGIN = "Whoops! Either username or password has not been filled in.";
+    String NO_USER_FOUND = "No user is registered under this username and password.";
+}
