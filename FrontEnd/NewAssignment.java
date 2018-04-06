f83f32354452134468efa016525e3c7e0b794163
Rylan Kettles <rkettles14@gmail.com>
2018-04-06 12:38:25 -0600

    bjkjjkj

--- FrontEnd/NewAssignment.java
+++ FrontEnd/NewAssignment.java
@@ -200,18 +200,6 @@ public class NewAssignment extends javax.swing.JDialog {
         //</editor-fold>
 
         /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                NewAssignment dialog = new NewAssignment(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
 //        java.awt.EventQueue.invokeLater(new Runnable() {
 //            public void run() {
 //                NewAssignment dialog = new NewAssignment(new javax.swing.JFrame(), true, );
